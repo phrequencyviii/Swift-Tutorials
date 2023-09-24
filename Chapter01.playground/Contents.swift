99d93d8eeab24998a71a280ac6e3aaaf350de6b3
Andrew Lawrence <70289843+phrequencyviii@users.noreply.github.com>
2023-09-23 21:05:25 -0400

    Initial commit

---
+++ Chapter01.playground/Contents.swift
@@ -0,0 +1,12 @@
+// This is a single line comment
+
+// This is a comment
+// over multiple lines
+
+/* This is also a comment
+ over multiple
+ lines */
+
+// print output to the terminal
+print("This is how you print output")
+
